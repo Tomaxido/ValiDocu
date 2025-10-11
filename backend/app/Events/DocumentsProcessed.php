bb1e869c690c21bd1079a6d66086161bb1b348aa
Francisco Manríquez <francisco.manriquezn@usm.cl>
2025-10-11 20:38:06 -0300

    Agregar evento DocumentsProcessed

---
+++ backend/app/Events/DocumentsProcessed.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class DocumentsProcessed implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $groupId;
+    public $userId;
+
+    public function __construct($groupId, $userId)
+    {
+        $this->groupId = $groupId;
+        $this->userId = $userId;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('documents.' . $this->userId);
+    }
+}
